b7380f4d7b0a4236f9a8663b6b961c1314d58dcd
hunnx27 <hunnx27@gmail.com>
2023-10-07 17:11:49 +0900

    add: 퍼블리싱 업데이트
    - 전체 삭제 후 일괄 업로드

--- src/main/resources/static/assets/js/common.js
+++ src/main/resources/static/assets/js/common.js
@@ -34,36 +34,93 @@ function tab(usetab){
         });
     });
 }
-
-
-window.onload = function(){}
-
-    /*
-    function selectCustom(){
-        var labels = document.querySelectorAll('.dropdown');
-        for (let index = 0; index < labels.length; index++) {
-            let options = labels[index].querySelectorAll('.dropdown-toggle');
-            let optionsList = labels[index].querySelectorAll('li');
-            console.log(optionsList)
-            labels[index].addEventListener('click', function(){
-                if(labels[index].classList.contains('on')) {
-                   labels[index].classList.remove('on');
-                } else {
-                    labels.forEach(function(lab){
-                        lab.classList.remove('on');
-                    })                    
-                    labels[index].classList.add('on');
-                }
+/* selectCustom function */
+function selectCustom(){
+    var labels = document.querySelectorAll('.dropdown');
+    for (let index = 0; index < labels.length; index++) {
+        let options = labels[index].querySelectorAll('.dropdown-toggle');
+        let optionsList = labels[index].querySelectorAll('li');
+        labels[index].addEventListener('click', function(){
+            if(labels[index].classList.contains('on')) {
+                labels[index].classList.remove('on');
+            } else {
+                labels.forEach(function(lab){
+                    lab.classList.remove('on');
+                });
+                labels[index].classList.add('on');
+                labels[index].focus();
+            }
+        })
+        
+        labels[index].querySelectorAll('li').forEach(function(lablesLi){
+            lablesLi.addEventListener('click',function(e){
+                let textVal = lablesLi.textContent;
+                lablesLi.parentNode.previousElementSibling.textContent = textVal
             })
-            labels[index].querySelectorAll('li').forEach(function(lablesLi){
-                lablesLi.addEventListener('click',function(e){
-                    let textVal = lablesLi.textContent;
-                    lablesLi.parentNode.previousElementSibling.textContent = textVal
-                })
+        })
+    
+
+    }
+    document.addEventListener('click',function(e){
+        if(e.target.parentNode.classList.contains('dropdown') == false && e.target.classList.contains('dropdown') == false){
+            labels.forEach(function(labelThis){
+                labelThis.classList.remove('on');
             })
         }
-    }selectCustom();
-    */
+    })
+}
+// textareaHeight()  -- textarea 텍스트 추가 자동크기변경
+function textareaHeight(){
+    const DEFAULT_HEIGHT = 32; // textarea 기본 height
+    var textareaNode = document.querySelectorAll('textarea');
+    textareaNode.forEach(function(textarea,idx){
+        textarea.style.height = 0;
+        textarea.style.height = 2 + textarea.scrollHeight + 'px'; 
+        textarea.addEventListener('keyup',function(e){
+            var $target = textareaNode[idx];
+            $target.style.height = 0;
+            $target.style.height = 2 + $target.scrollHeight + 'px';
+        });
+    });
+}
+/* all checked */
+function boardAllChecked(boardcheck){
+    console.log(boardcheck.closest('.table'))
+    var useTable = boardcheck.closest('.table');
+    var useTableTr = useTable.querySelector('tbody');
+    var checkboxs = useTableTr.querySelectorAll('input[type="checkbox"]')
+    if(boardcheck.checked == true){
+        checkboxs.forEach(function(checkbox){
+            checkbox.checked = true;
+        })
+    }else{
+        checkboxs.forEach(function(checkbox){
+            checkbox.checked = false;
+        })
+    }
+}
+
+
+window.onload = function(){
+    selectCustom();
+    //textareaHeight();
+    // all check handler
+    var allcheckbox = document.querySelectorAll('.board-all-check');
+    allcheckbox.forEach(function(checkThis){
+        checkThis.addEventListener('click',function(){
+            boardAllChecked(checkThis);
+        })
+        
+    })
+}
+  
+    
+    
     
     /* tab event */
     
+
+    
+
+   
+    //modifyBoard('.board-list-FPL');
