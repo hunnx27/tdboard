c73af97542a3129198270c06801a802579339e4c
rosa <jiyeonleezzang.dev@gmail.com>
2024-01-03 00:02:43 +0900

    upt. notice 리스트 api 연동

--- src/main/resources/static/assets/js/home/notice.js
+++ src/main/resources/static/assets/js/home/notice.js
@@ -1,47 +1,43 @@
 import createPaginationModule from "/assets/js/pagination.js";
+import useAxios from '/assets/js/api/useAxios.js'
+import useFilters from '/assets/js/useFilters.js'
+
 const paginationModule = createPaginationModule();
-$(async function(){
-    var data = { name: "Jane", 
-                 items: [
-                        {no:1, id:1, title: 'K-디지털 플랫폼 홈페이지 개설', date: '2013.12.20'}, 
-                        {no:2, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:3, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:4, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:5, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:6, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:7, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:8, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:9, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                        {no:10, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                    ] ,
-                totalCount: 11
-               };
-    var data2 = { name: "Jane", 
-                items: [
-                        {no:11, id:2, title: 'K-디지털 플랫폼 홈페이지 개설22', date: '2013.12.20'},
-                    ] ,
-                totalCount: 11
-            };
-    // api call 한 다음 결과값 아래 세팅
-    handleSetList(1, data)
-    
-     paginationModule.setClickPageNumberHandler((pageNumber)=> {
+$(function(){
+
+    getNoticeApi(1)
+ 
+    paginationModule.setClickPageNumberHandler((pageNumber)=> {
         console.log(`Page number clicked: ${pageNumber}`);
-        if(pageNumber=== 2){
-            handleSetList(pageNumber, data2)
-        }else {
-            handleSetList(1, data)
-        }
-        
-   
+        getNoticeApi(pageNumber)
      })
-    
 })
+async function getNoticeApi(pageNumber){
+    await useAxios.get('/api/v1/boards/type/notices',
+        {page: 1}
+        ,(res)=> {
+        // console.log('res',res.data)
+        if(res.data.totalElements > 0){
+            res.data.content.map((data)=>{
+                data.createdDateText = useFilters().YYYYMMDD(data.createdDate)
+            })
+            handleSetList(pageNumber, res.data)
+        }else {
+            paginationModule.setPage(1,[])
+        }
+        const tableHeadText = `Total ${res.data.totalElements}건 ${res.data.totalPages} 페이지`
+        document.getElementById("tableHeadText").innerHTML = tableHeadText
+
+    },(err)=> {
+        console.log('err',err)
+    })
+
+}
 
 function handleSetList(pageNumber, data){
     var template = document.getElementById("notice-table-template").innerHTML;
     var result = Mustache.render(template, data);
     document.getElementById("notice-body").innerHTML = result;
     
-    paginationModule.setPage(pageNumber,data.totalCount)
+    paginationModule.setPage(pageNumber,data.totalElements)
 }
