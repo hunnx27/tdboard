6b8e0c4fc821b5af16f3d2e5ff9330c0ad5ba768
rosa <jiyeonleezzang.dev@gmail.com>
2024-01-21 21:50:46 +0900

    upt. 아이디찾기 api 추가

--- src/main/resources/static/assets/js/home/auth/id-find.js
+++ src/main/resources/static/assets/js/home/auth/id-find.js
@@ -1,5 +1,7 @@
+import useAxios from '/assets/js/api/useAxios.js'
+
 window.onload = function() {
-    const name = document.getElementById('name')
+    const name = document.getElementById('username')
     const email = document.getElementById('email')
     const idFindConfirm = document.getElementById('idFindConfirm');
    
@@ -19,8 +21,25 @@ window.onload = function() {
     })
 
     idFindConfirm.addEventListener('click', function(){
-        // api call
-        alert('success')
+        handleSearchId()
+        
     })
 
+}
+
+async function handleSearchId() {
+    const name = document.getElementById('username').value
+    const email = document.getElementById('email').value
+    await useAxios.post('/api/v1/auth/find/id',
+    {
+        "username": name,
+        "email": email
+    }
+    ,(res)=> {
+        alert('찾으시는 아이디는 '+res.data+' 입니다.')
+      
+    },(err)=> {
+        // console.log('err',err)
+        alert(err.response.data.message)
+    })
 }
