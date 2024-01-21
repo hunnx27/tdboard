d2f368e7a060da9b3d4867376811e2e7131097e6
rosa <jiyeonleezzang.dev@gmail.com>
2024-01-21 22:01:33 +0900

    upt. 비밀번호 찾기 api 추가

--- src/main/resources/static/assets/js/home/auth/password-find.js
+++ src/main/resources/static/assets/js/home/auth/password-find.js
@@ -1,3 +1,5 @@
+import useAxios from '/assets/js/api/useAxios.js'
+
 window.onload = function() {
     const userId = document.getElementById('userId')
     const userName = document.getElementById('userName')
@@ -28,8 +30,27 @@ window.onload = function() {
     })
 
     pwdFindConfirm.addEventListener('click', function(){
-        // api call
-        alert('success')
+        
+        handleSearchPwd()
     })
 
+}
+
+async function handleSearchPwd() {
+    const userId = document.getElementById('userId').value
+    const userName = document.getElementById('userName').value
+    const email = document.getElementById('email').value
+    await useAxios.post('/api/v1/auth/find/id',
+    {
+        "username": userName,
+        "email": email,
+        "userId": userId
+    }
+    ,(res)=> {
+        alert('초기화 비밀번호는 가입했던 휴대전화번호입니다.')
+        location.reload()
+    },(err)=> {
+        // console.log('err',err)
+        alert(err.response.data.message)
+    })
 }
