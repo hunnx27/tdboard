912bae6420d8b31d854f8117a6be8ed2b7214b09
rosa <jiyeonleezzang.dev@gmail.com>
2024-01-21 19:00:00 +0900

    upt. 시설 예약 상세 퍼블, 로직 적용

--- src/main/resources/static/assets/js/home/contents/facilities-detail.js
+++ src/main/resources/static/assets/js/home/contents/facilities-detail.js
@@ -6,20 +6,21 @@ import useFilters from '/assets/js/useFilters.js'
 window.onload = function () {
     const facilitiesId = document.getElementById('facilitiesId').value
 
-    getEquipmentsApi(facilitiesId)
+    getFacilitiesApi(facilitiesId)
 
     initElementSetting()
 };
 
-async function getEquipmentsApi(id){
+async function getFacilitiesApi(id){
     await useAxios.get(`/api/v1/facilities/${id}`,
         {page: 1}
         ,(res)=> {
-        // console.log('res',res.data)
+        console.log('res',res.data)
         if(res.data){
-            var template = document.getElementById("facilities-detail-body").innerHTML;
-            var result = Mustache.render(template, res.data);
-            document.getElementById("facilities-detail-body").innerHTML = result;
+            const facilitiesImg = document.getElementById('facilitiesImg')
+            facilitiesImg.setAttribute('src',res.data.imageUrl)
+            const facilitiesTitle = document.getElementById('facilitiesTitle')
+            facilitiesTitle.innerText = res.data.name
             
             const applicationBtn = document.getElementById("applicationBtn")
             applicationBtn.addEventListener('click', () =>{
@@ -37,31 +38,67 @@ function initElementSetting(){
     // 현재 년도 가져오기
     const currentYear = new Date().getFullYear();
 
-    // select 요소 가져오기
-    const yearSelect = document.getElementById('yearSelect');
-    const monthSelect = document.getElementById('monthSelect');
-    const daySelect = document.getElementById('daySelect');
-
     // 옵션 추가: 현재 년도부터 현재 년도 + 2년까지
+    const yearOptionData = []
     for (let year = currentYear; year <= currentYear + 2; year++) {
-        const option = document.createElement('option');
-        option.value = year;
-        option.text = year;
-        yearSelect.appendChild(option);
+        yearOptionData.push({
+            'value' : year,
+            'text' : year
+        });
     }
+
+    var template = document.getElementById("select-option-template").innerHTML;
+    var result = Mustache.render(template, {options: yearOptionData});
+    document.getElementById("yearSelectOption").innerHTML = result;
+
+
     // 옵션 추가: 1월부터 12월까지
+    const monthOptionData = []
     for (let month = 1; month <= 12; month++) {
-        const option = document.createElement('option');
-        option.value = month;
-        option.text = month;
-        monthSelect.appendChild(option);
+        monthOptionData.push({
+            'value' : month,
+            'text' : month
+        });
     }
+
+    var template = document.getElementById("select-option-template").innerHTML;
+    var result = Mustache.render(template, {options: monthOptionData});
+    document.getElementById("monthSelectOption").innerHTML = result;
+
 
     // 옵션 추가: 1일부터 31일까지
+    const dayOptionData = []
     for (let day = 1; day <= 31; day++) {
-        const option = document.createElement('option');
-        option.value = day;
-        option.text = day;
-        daySelect.appendChild(option);
+        dayOptionData.push({
+            'value' : day,
+            'text' : day
+        });
     }
+    var template = document.getElementById("select-option-template").innerHTML;
+    var result = Mustache.render(template, {options: dayOptionData});
+    document.getElementById("daySelectOption").innerHTML = result;
+
+    // 옵션 추가: 00부터 23까지
+    const hourOptionData = []
+    for (let hour = 0; hour <= 23; hour++) {
+        hourOptionData.push({
+            'value' : hour,
+            'text' : hour
+        });
+    }
+    var template = document.getElementById("select-option-template").innerHTML;
+    var result = Mustache.render(template, {options: hourOptionData});
+    document.getElementById("hourSelectOption").innerHTML = result;
+
+    // 옵션 추가: 0분, 30분
+    const minuteOptionData = []
+    for (let minute = 0; minute <= 30; minute += 30) {
+        minuteOptionData.push({
+            'value' : minute,
+            'text' : minute
+        });
+    }
+    var template = document.getElementById("select-option-template").innerHTML;
+    var result = Mustache.render(template, {options: minuteOptionData});
+    document.getElementById("minuteSelectOption").innerHTML = result;
 }
