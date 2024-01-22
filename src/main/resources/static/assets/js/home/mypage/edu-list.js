a0cdbb023c9ec5627180d8a8abed33558741bf7f
rosa <jiyeonleezzang.dev@gmail.com>
2024-01-22 23:16:09 +0900

    upt. 마이페이지 > 교육이력 리스트, 취소 api 추가

--- src/main/resources/static/assets/js/home/mypage/edu-list.js
+++ src/main/resources/static/assets/js/home/mypage/edu-list.js
@@ -13,13 +13,16 @@ $(function(){
      })
 })
 async function getBoardApi(pageNumber){
-    await useAxios.get('/api/v1/boards/type/notices',
+    await useAxios.get('/api/v1/users/me/applications',
         {page: pageNumber}
         ,(res)=> {
         if(res.data.paging.totalElements > 0){
             let firstPostNumber = res.data.paging.totalElements - (pageNumber - 1) * res.data.paging.pageSize;
             res.data.contents.map((data)=>{
-                data.createdDateText = useFilters().YYYYMMDD(data.updatedAt || data.createdDate)
+                data.notApproval = data.approvalYn === 'N' && !data.approvalAt ? true : false // 승인대기
+                data.approval = data.approvalYn === 'Y' && data.approvalAt ? true : false //승인완료
+                data.cancelApplication = data.approvalYn === 'N' && data.approvalAt ? true : false //취소완료
+                data.createdDateText = useFilters().YYYYMMDD(data.updatedAt || data.createdDate, 'YYYY-MM-DD HH:mm:ss')
                 data.no = firstPostNumber
                 firstPostNumber--;
             })
@@ -30,6 +33,18 @@ async function getBoardApi(pageNumber){
         const tableHeadText = `Total ${res.data.paging.totalElements}건 ${res.data.paging.totalPages} 페이지`
         document.getElementById("tableHeadText").innerHTML = tableHeadText
 
+
+
+        const applicationCancelBtn = document.querySelector('.applicationCancelBtn')
+        if(applicationCancelBtn){
+            applicationCancelBtn.addEventListener('click', function() {
+                // 클릭한 버튼의 data-value 속성 값 가져오기
+                const dataValue = this.getAttribute('data-value');
+        
+                // 취소 함수 호출
+                handleApplicationCancel(dataValue);
+            });
+        }
     },(err)=> {
         console.log('err',err)
     })
@@ -44,3 +59,15 @@ function handleSetList(pageNumber, data){
     paginationModule.setPage(pageNumber,data.paging.pageSize,data.paging.totalElements)
 }
 
+// 신청 취소
+async function handleApplicationCancel(id){
+    await useAxios.delete(`/api/v1/users/me/applications/${id}`,
+    {}
+        ,(res)=> {
+            // console.log(res.data)
+            alert('취소 되었습니다.')
+            location.reload()
+    },(err)=> {
+        alert(err.response.data.message)
+    })
+}
